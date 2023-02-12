01b93d5ec44f2ac6889637ba6f85faaf2578787d
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-02-12 16:25:21 +0700

    📦 Modify `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use image_ascii_converter::ASCIIConverter;
+
 fn main() {
     println!("Hello, world!");
+    let converter = ASCIIConverter::new("dummy.jpg", "output.png", 32, 1);
+    converter.convert_to_img().unwrap();
+    // let result = converter.convert_to_str().unwrap();
+    // println!("{}", result);
 }
